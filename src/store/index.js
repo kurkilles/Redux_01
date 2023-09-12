8eac74d2357b1aeea44d6894837f0c6adcfcfb42
kurkilles <mikolajdemi@gmail.com>
2023-09-12 16:24:21 +0200

    Multiple slices

--- src/store/index.js
+++ src/store/index.js
@@ -22,10 +22,24 @@ const counterSlice = createSlice({
     }
 });
 
+const authSlice = createSlice({
+    name: "auth",
+    initialState: { isAuthenticated: false },
+    reducers: {
+        login(state) {
+            state.isAuthenticated = true;
+        },
+        logout(state) {
+            state.isAuthenticated = false;
+        }
+    }
+});
+
 
 const store = configureStore({
-    reducer: counterSlice.reducer
+    reducer: { counter: counterSlice.reducer, auth: authSlice.reducer }
 });
 
 export const counterActions = counterSlice.actions;
+export const authActions = authSlice.actions;
 export default store;
